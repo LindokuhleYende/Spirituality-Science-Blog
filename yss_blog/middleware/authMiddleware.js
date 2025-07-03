b3fda4af9d72c43e440367c09f0baac211ef8261
Lindokuhle <yendelindokuhle2@gmail.com>
2025-07-03 11:24:37 +0200

    Update authMiddleware.js

--- yss_blog/middleware/authMiddleware.js
+++ yss_blog/middleware/authMiddleware.js
@@ -1,10 +1,16 @@
-const User = require("../models/User")
+const User = require("../models/User");
 
-module.exports = (req, res, next) => {
-    User.findById(req.session.userId, (error, user) => {
-        if (error || !user)
+module.exports = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.session.userId);
+
+        if (!user) {
             return res.redirect('/');
-        
-            next();
-    });
-}
+        }
+
+        next();
+    } catch (error) {
+        console.error("Auth Middleware Error:", error);
+        return res.redirect('/');
+    }
+};
