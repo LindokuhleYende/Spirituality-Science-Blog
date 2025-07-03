3214db2d4a7dd0047c60227567bc0fec6e0e2e52
Lindokuhle <yendelindokuhle2@gmail.com>
2025-07-03 11:24:33 +0200

    Update storeUser.js

--- yss_blog/controllers/storeUser.js
+++ yss_blog/controllers/storeUser.js
@@ -2,15 +2,17 @@ const User = require('../models/User.js')
 const path = require('path')
 
 
-module.exports = (req, res) => {
-    User.create(req.body, (error, user) => {
-        if (error) {
-            const validationErrors = Object.keys(error.errors).map(key =>
-                error.errors[key].message)
-            req.flash('validationErrors', validationErrors)
-            req.flash('data',req.body)
-            return res.redirect('/auth/register')
-        }
-        res.redirect('/')
-    })
-}
+module.exports = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.redirect('/');
+    } catch (error) {
+        const validationErrors = Object.keys(error.errors || {}).map(
+            (key) => error.errors[key].message
+        );
+        req.flash('validationErrors', validationErrors);
+        req.flash('data', req.body);
+        return res.redirect('/auth/register');
+    }
+};
+
