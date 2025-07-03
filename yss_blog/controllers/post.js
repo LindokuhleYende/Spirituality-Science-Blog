5289824d08e8a5b63543599722b2a3099545a826
Lindokuhle <yendelindokuhle2@gmail.com>
2025-07-03 11:24:29 +0200

    Update post.js

--- yss_blog/controllers/post.js
+++ yss_blog/controllers/post.js
@@ -1,20 +1,25 @@
-const BlogPost = require("../models/BlogPost")
+const BlogPost = require("../models/BlogPost");
 
 module.exports = {
-    delete: (req, res) => {
-        let postId = req.params.id;
-        BlogPost.findByIdAndRemove(postId)
-            .then(() => {
-                res.redirect("/");
-            })
-            .catch(error => {
-                console.log(`Error deleting post by ID: ${error.message}`);
-            });
+    delete: async (req, res) => {
+        try {
+            const postId = req.params.id;
+            await BlogPost.findByIdAndDelete(postId);
+            res.redirect("/");
+        } catch (error) {
+            console.error(`Error deleting post by ID: ${error.message}`);
+            res.status(500).send("Failed to delete post");
+        }
     },
 
     redirectView: (req, res) => {
         let redirectPath = res.locals.redirect;
-        if (redirectPath !== undefined) res.redirect(redirectPath);
-        else console.log(error);
+        if (redirectPath !== undefined) {
+            res.redirect(redirectPath);
+        } else {
+            console.error("Missing redirect path");
+            res.status(500).send("No redirect path defined");
+        }
     },
-}
+};
+
